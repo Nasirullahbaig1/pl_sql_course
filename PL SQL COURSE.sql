713f47e2f26ad3d19b66fc2d9a93728e3f221f84
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-12-15 19:58:58 +0500

    Update PL SQL COURSE.sql

--- PL SQL COURSE.sql
+++ PL SQL COURSE.sql
@@ -1,1 +1,12 @@
 --Introduction to PL-SQL ORACLE
+--Procedural launage in SQL
+
+--PL/SQL Anonymous block 
+--Decleration section
+--Begin
+--Execution sectoin
+--Exception section
+--END.
+
+--Iteration (LOOP)
+--Handeling exceptions
