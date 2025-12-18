b7b42aadd476ba32031703708916b5418668f2b9
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-12-18 19:25:11 +0500

    Update PL SQL COURSE.sql

--- PL SQL COURSE.sql
+++ PL SQL COURSE.sql
@@ -31,11 +31,17 @@
 --DECLARE
 --variable declaration goes here
 
+SET SERVEROUTPUT ON;
 DECLARE
+    V_NUM_D NUMBER DEFAULT 10;
     V_NUM NUMBER;
-    V_VARCAHR2 VARCHAR2(100);
+    V_VARCAHR2 VARCHAR2(100) NOT NULL := 'THIS IS DEFALUT VALUE';
     V_EMP_ID employees.emp_id%TYPE;
     V_EMP_FIRSTNAME employees.firstname%TYPE;
+BEGIN
+    dbms_output.put_line('V_NUM_D);
+END;
+/
     
 
 
