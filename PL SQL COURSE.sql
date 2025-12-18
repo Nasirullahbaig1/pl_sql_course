9c42890fae8bfe06b5e3469aab7047f33c73b088
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-12-18 19:57:25 +0500

    Update PL SQL COURSE.sql

--- PL SQL COURSE.sql
+++ PL SQL COURSE.sql
@@ -36,10 +36,10 @@ DECLARE
     V_NUM_D NUMBER DEFAULT 10;
     V_NUM NUMBER;
     V_VARCAHR2 VARCHAR2(100) NOT NULL := 'THIS IS DEFALUT VALUE';
-    V_EMP_ID employees.emp_id%TYPE;
-    V_EMP_FIRSTNAME employees.firstname%TYPE;
+    --V_EMP_ID CONSTANT CATEGORIES.CATEGORYID%TYPE :=12;
+    --V_EMP_FIRSTNAME NAME.FULLNAME%TYPE;
 BEGIN
-    dbms_output.put_line('V_NUM_D);
+    dbms_output.put_line('V_NUM_D' || ' = ' || V_NUM_D);
 END;
 /
     
