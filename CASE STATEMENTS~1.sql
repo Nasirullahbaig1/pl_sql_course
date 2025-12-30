1314de92078475e2963f93cc744a872bb9017ab0
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-12-30 22:30:36 +0500

    Create CASE STATEMENTS~1.sql

---
+++ CASE STATEMENTS~1.sql
@@ -0,0 +1,11 @@
+--CASE STATEMENTS
+--Type of case statements
+
+--SIMPLE CASE STATEMENT:
+--      CASE EXPRESSION
+--      WHEN VALUE THEN STATEMENT
+--      WHEN VALUE THEN STATMENNT
+--      ...
+--      END;
+
+--
