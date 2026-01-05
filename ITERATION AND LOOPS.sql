bb07952f199b04a1eec8ba7229cf4dd84f3afcbb
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-01-05 20:11:46 +0500

    Update ITERATION AND LOOPS.sql

--- ITERATION AND LOOPS.sql
+++ ITERATION AND LOOPS.sql
@@ -2,3 +2,8 @@
 
 --BASIC LOOP STATEMENT
 --Basic loop consist of LOOP, END LOOP AND EXIST statement.
+--SYNTAX
+--<<LABEL_NAME>> LOOP
+--      <execute statements>
+--      <EXIST{LABLE_NAME} OR EXIST{LABLE_NAME} WHEN>
+--    END LOOP;
