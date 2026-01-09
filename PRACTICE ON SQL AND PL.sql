e0e851fe3b6c70d6b4a5ae5f520f984ecfeab481
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-01-09 19:49:16 +0500

    Create PRACTICE ON SQL AND PL.sql

---
+++ PRACTICE ON SQL AND PL.sql
@@ -0,0 +1,21 @@
+CREATE TABLE emp_2 (
+    emp_name    VARCHAR(20),
+    emp_id      NUMBER(5),
+    ta          NUMBER(15),
+    da          NUMBER(15),
+    total       NUMBER(20),
+    branch_city VARCHAR(15)
+)
+
+INSERT INTO EMP_2 VALUES('ABC', 10, 1200, 1340, NULL, 'LAHORE');
+INSERT INTO EMP_2 VALUES('XYZ', 11, 1300, 1200, NULL, 'KARACHI');
+INSERT INTO EMP_2 VALUES('DEF', 12, 1500, 1640, NULL, 'ISLAMABAD');
+
+SELECT * FROM EMP_2;
+
+
+
+
+
+
+
