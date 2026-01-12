fc7c27aaaf6bba1417de65043810f1d1680519d6
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-01-12 20:08:10 +0500

    Update EXCEPTION_HANDELING.sql

--- EXCEPTION_HANDELING.sql
+++ EXCEPTION_HANDELING.sql
@@ -1,2 +1,17 @@
 --EXCEPTOIN HANDELING
---
+--When we have some error in the code we can handle that through exception block.
+
+--Assignment:
+--Create a plsql block to fatch employee having firstname 'nasir' and show its salary,
+--> if not avalible then show the heighest salary using EXCEPTION BLOCK.
+SELECT * FROM EMPLOYEES;
+
+SET SERVEROUT ON;
+DECLARE
+    V_NAME EMPLOYEES.FIRST_NAME%TYPE;
+    V_SALARY EMPLOYEES.SALARY%TYPE;
+BEGIN
+    SELECT FIRST_NAME, SALARY INTO V_NAME, V_SALARY FROM EMPLOYEES WHERE FIRST_NAME = 'Steven';
+    DBMS_OUTPUT.PUT_LINE('NAME --> ' ||V_NAME || ' SALARY --> '|| v_SALARY);
+END;
+/
