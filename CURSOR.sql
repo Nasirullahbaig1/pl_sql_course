9936503658c0d6729971938e52ba4d8812adf9c5
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-01-26 22:57:17 +0500

    Update CURSOR.sql

--- CURSOR.sql
+++ CURSOR.sql
@@ -7,3 +7,8 @@
 --it also throws NO_DATA_FOUND and TOO_MANY_ROWS as oracle handle implicit cursor in the standard way.
 
 --Explicit Cursor:
+--we can declare the cursor as follows
+--DECLARE
+--  CURSOR <cursor_name> IS <query>
+--cursor should be opened before using it. When we openthe cursor it execte the querry assosiated with it. 
+-- OPEN <cursor_name>
