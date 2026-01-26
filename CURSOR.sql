83130bca3149f6265269a374d7710b90fd8d9909
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-01-26 22:42:46 +0500

    Create CURSOR.sql

---
+++ CURSOR.sql
@@ -0,0 +1,9 @@
+--CURSOR
+--CURSOR IS A POINTER TO THE QUERRY.
+--There are two type of Cursor
+
+--Implicit Cursor:
+--Implicit cursor is created and used by Oracle.
+--it also throws NO_DATA_FOUND and TOO_MANY_ROWS as oracle handle implicit cursor in the standard way.
+
+--Explicit Cursor:
