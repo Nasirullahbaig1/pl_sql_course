5981066b9813c15d4ff5ac703da9715cbcdcf1d9
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-01-28 20:20:14 +0500

    Create PROCEDURES~1.sql

---
+++ PROCEDURES~1.sql
@@ -0,0 +1,17 @@
+--PROCEDURE IN PL/SQL
+--PL/SQL  procedures are reusable code block that perform specific actions or logic within a database environment.
+--Mainly it has two parts PROCEDURE HEADER and PROCEDURE BODY.
+
+--There are two type of procedures
+--LOCAL PROCEDURE(ANNOYMOUS OR UNNAMED):
+    --As it local it execute in the same session due to which
+        --Intialization of procedure
+        --Calling of procedure
+    --To be done in same program
+    
+--STORED PROCEDURE(NAMED PROCEDURE):
+    --As it stored so it can be stored & can easily executes later on
+        --Initilization of procedure
+        --Calling of procedure
+    --Can be done differently
+
