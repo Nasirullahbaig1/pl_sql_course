bb28c0647f2067885502e94592085784f0f4522b
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-02-03 02:01:00 +0500

    Create FUNCTIONS.sql

---
+++ FUNCTIONS.sql
@@ -0,0 +1,6 @@
+--FUNCTIONS IN PL/SQL
+
+--Function is the stored pl/sql block which can accept and return the values to the calling code same as procedure.
+--Function syntax:
+--CREATE [OR REPLACE] FUNCTION function_name
+--(input/output parameter list) RETURN data_type     
