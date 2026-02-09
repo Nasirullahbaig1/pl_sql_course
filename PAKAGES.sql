456e18dd735044336523b1f156dff81c934ddadf
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-02-09 20:48:22 +0500

    Update PAKAGES.sql

--- PAKAGES.sql
+++ PAKAGES.sql
@@ -60,6 +60,12 @@ CREATE OR REPLACE PACKAGE pkg_fisrt_package AS
 END pkg_fisrt_package;
 /
 
+--If the paakages specification has cursors or sub-programs, then the packages body is mendatory.
+--Both the package body and package specification must be in the same schema.
+--Each cursor or sub-program declared in the package specification must have implementation in the package body.
+--Package body can have additional curosr or sub-progarm which is private to package body and accessible to only package body.
+
+
 
 
 
