6fd160eadd15cc83cfa3c37df89fb25966e40dea
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-02-12 01:21:37 +0500

    Create TRIGGERS.sql

---
+++ TRIGGERS.sql
@@ -0,0 +1,6 @@
+--TRIGGER IN PL/SQL
+
+--What is trigger?
+--Trigger is stored in pl/sql block which is executed when some event occurs.
+--There can be multiple events on which trigger can be created.
+
