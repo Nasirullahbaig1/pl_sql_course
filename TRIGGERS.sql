56e04bca0499dc54c65351b0684ae05dc9dd7617
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-02-12 21:47:44 +0500

    Update TRIGGERS.sql

--- TRIGGERS.sql
+++ TRIGGERS.sql
@@ -36,6 +36,15 @@
 --FOLLOWS or PRECEDES another trigger is used to define the sequaence of trigger in case multiple trigger is created on the same event.
 --WHEN clause is used to define the condition on which trigger should be executed.
 
+--ROW LEVEL TRIGGERS
+--row level trigger executes for each row affected. ie If one statement update 50 records then trigger will be executed 50 times
+--row level trigger mostly needed when we want to work on data level activity.
+--:OLD and :NEW is used to find the old and new values of the data as :OLD.column_name respectively.
+--Colon is used before OLD and NEW as they are external varaible references.
+--:OLD .column_name is null for INSERT event and :NEW.column_name is null for delecte event.
+--Using REFERENCING keyword OLD and NEW keyword can be override with another names.
+--Exceptions are used to ristrict the event in trigger based on condition.
+--RAISE_APPLICATION_ERROR is mostly used to proveide the proper information and it automatically rollback the transaction.
 
 
 
