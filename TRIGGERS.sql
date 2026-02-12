7c5169a2e977e7cf15e1fba50e43fb59f85a7c71
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-02-12 20:41:38 +0500

    Update TRIGGERS.sql

--- TRIGGERS.sql
+++ TRIGGERS.sql
@@ -14,3 +14,56 @@
     --SHOTDOWN OR STARTUP
 --USER EVENTS
     --LOGIN OR LOGOUT
+--Triggers are generally used to implement complex login, complex restriction, audits,generate column values automatically.
+
+--SYNTAX:
+
+--CREATE [OR REPLACE] TRIGGER trigger_name
+--[BEFORE | AFTER] triggering_event ON table_name
+--[FOR EACH ROW]
+--[WHEN condition]
+--DECLARE
+--      .....
+--BEGIN
+--      .....
+--EXCEPTION
+--      .....
+--END;
+
+--BEFORE or AFTER keyword specifies when to execute the trigger, before the triggering event or after.
+--FOR EACH ROW specifies that trigger is row level trigger. row level trigger executed once for each row changed/inserted/deleted.
+--statement level trigger is the default trigger type and it is executed per statemnt regardless of number of records affected.
+--FOLLOWS or PRECEDES another trigger is used to define the sequaence of trigger in case multiple trigger is created on the same event.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
