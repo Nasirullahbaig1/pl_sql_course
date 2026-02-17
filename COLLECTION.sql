4a55ae2bfc7294372e1bc5e81da6170aeded226f
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-02-17 22:57:10 +0500

    COLLECTION

---
+++ COLLECTION.sql
@@ -0,0 +1,10 @@
+--COLLECTION IN PL/SQL
+
+--PL/SQL  Associative array is
+--1. Single dimension --> Single column of data in data in each row.
+--2. Sparse --> Elements are not continuous/sequential.
+    --There can be gaps bewteen element.
+--3. Unbounded --> Number of elements in the array is not predefined.
+
+--Associative array can be indexed by BINARY_INTEGER, PLS_INTERGER, or VARCHAR2.
+
