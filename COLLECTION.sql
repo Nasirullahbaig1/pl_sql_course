b1ace4f3d936823e695c9447f2fee669f2912e40
Nasir Ullah Baig <baignasirullah1@gmail.com>
2026-02-23 21:01:22 +0500

    Update COLLECTION.sql

--- COLLECTION.sql
+++ COLLECTION.sql
@@ -7,8 +7,16 @@
 --3. Unbounded --> Number of elements in the array is not predefined.
 
 --Associative array can be indexed by BINARY_INTEGER, PLS_INTERGER, or VARCHAR2.
-   
-   
+SET SERVEROUT ON;
+DECLARE
+    TYPE TYPE_AA_DEPT IS TABLE OF VARCHAR2(50) INDEX BY VARCHAR2(20);
+    V_TYPE_AA_DEPT TYPE_AA_DEPT;
+BEGIN
+    V_TYPE_AA_DEPT('DEPARTMENT1') := 'JAMU';
+    V_TYPE_AA_DEPT('DEPARTMENT2') := 'LAHORE';
+    
+    DBMS_OUTPUT.PUT_LINE(V_TYPE_AA_DEPT('DEPARTMENT1'));
+END;
    
    
    
